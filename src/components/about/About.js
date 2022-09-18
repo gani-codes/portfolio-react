b010a13a603267801d0e2d348bc59255ec712ac3
Gani <abdulgani.work@gmail.com>
2022-09-18 23:52:35 +0530

    about section added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,48 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { BiCodeCurly } from "react-icons/bi";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 export default function about() {
-  return <section id="about"></section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__card-icon" />
+              <h5>Experience</h5>
+              <small>1+ Years</small>
+            </article>
+            <article className="about__card">
+              <AiOutlineFundProjectionScreen className="about__card-icon" />
+              <h5>Projects</h5>
+              <small>3+ Done</small>
+            </article>
+            <article className="about__card">
+              <BiCodeCurly className="about__card-icon" />
+              <h5>Skills</h5>
+              <small>React</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis
+            voluptatem eveniet ipsam exercitationem repellat nam reprehenderit
+            qui cumque ea, accusantium quo porro soluta voluptates officia eos
+            vero. Temporibus, optio quod.
+          </p>
+          <button className="btn btn-primary">Let's talk</button>
+        </div>
+      </div>
+    </section>
+  );
 }
